e0d37d05a7388c2bcacc8a654a4c86a12f9aff9e
root <root@localhost.localdomain>
2018-09-04 08:41:50 -0400

    Change function msr and cpuid to callable object

--- cpuid.py
+++ cpuid.py
@@ -6,28 +6,38 @@ Depends:
 import os
 import struct
 
-def cpuid(cpu, EAX):
-    """Execute CPUID function specified by EAX on CPU and return the result.
-    
-    This function will return the result of the specified CPUID function.
-   
-    Args:
-        cpu: [integer] specifies CPU on which the CPUID execute.
-        EAX: [integer] specifies the CPUID function.
-        
-    Returns:
-        The result of the CPUID function in term of (EAX, EBX, ECX, EDX).
-
-    Exceptions:
-        FileNotFoundError: 'msr' not loaded or CPU index out of range.
-        PermissionError: run this module as normal user.
+class CpuId(object):
+    """CpuId(integer) -> Callable object to execute CPUID on CPU specified by 
+    integer.
     """
+    def __init__(self, cpu):
+        self.cpu = cpu
+        self.fd = None
+    def __call__(self, EAX):
+        """Execute CPUID function specified by EAX and return the result.
+        
+        This function will return the result of the specified CPUID function.
+       
+        Args:
+            EAX: [integer] specifies the CPUID function.
+            
+        Returns:
+            The result of the CPUID function in term of (EAX, EBX, ECX, EDX).
     
-    file_path = "/dev/cpu/%d/cpuid" % cpu
-    flags = os.O_RDONLY
-    fd = os.open(file_path, flags)
-    os.lseek(fd, EAX, os.SEEK_SET)
-    value_str = os.read(fd, 16)
-    (A, B, C, D) = struct.unpack('<4I', value_str)
-    os.close(fd)
-    return (A, B, C, D)
+        Exceptions:
+            FileNotFoundError: 'msr' not loaded or CPU index out of range.
+            PermissionError: run this module as normal user.
+        """
+        if self.fd is None:
+            file_path = "/dev/cpu/%d/cpuid" % self.cpu
+            flags = os.O_RDONLY
+            self.fd = os.open(file_path, flags)
+        os.lseek(self.fd, EAX, os.SEEK_SET)
+        value_str = os.read(self.fd, 16)
+        (A, B, C, D) = struct.unpack('<4I', value_str)
+        return (A, B, C, D)
+    def __del__(self):
+        try:
+            os.close(self.fd)
+        except Exception:
+            pass
--- msr.py
+++ msr.py
@@ -6,35 +6,41 @@ Depends:
 import os
 import struct
 
-def msr(cpu,  index,  value=None):
-    """Read or write MSR [index] of CPU specified by cpu.
-    
-    This function will return the value of the specified MSR. If value is
-    specified, the MSR will be updated with value first.
-   
-    Args:
-        cpu: [integer] specify the MSR of which CPU.
-        index: [integer] specify the MSR index.
-        value: [integer] new MSR value.
+class Msr(object):
+    """Msr(integer) -> Callable object to R/W MSR of CPU specified by integer."""
+    def __init__(self, cpu):
+        self.cpu = cpu
+        self.fd = None
+    def __call__(self, index, value=None):
+        """Msr(index) -> Read MSR [index] and return;
+        Msr(index, value) -> Write value to MSR[index], then read and return.
         
-    Returns:
-        The value of specified MSR.
-
-    Exceptions:
-        FileNotFoundError: 'msr' not loaded or CPU index out of range.
-        PermissionError: run this module as normal user.
-    """
+        This function will return the value of the specified MSR. If value is
+        specified, the MSR will be updated with value first.
+       
+        Args:
+            index: [integer] specify the MSR index.
+            value: [integer] new MSR value.
+            
+        Returns:
+            The value of specified MSR.
     
-    file_path = "/dev/cpu/%d/msr" % cpu
-    if value is None:
-        flags = os.O_RDONLY
-    else:
-        flags = os.O_RDWR
-    fd = os.open(file_path,  flags)
-    os.lseek(fd,  index,  os.SEEK_SET)
-    if value is not None:
-        os.write(fd,  value)
-    value_str = os.read(fd,  8)
-    (msr_value, ) = struct.unpack('<Q',  value_str)
-    os.close(fd)
-    return msr_value
+        Exceptions:
+            FileNotFoundError: 'msr' not loaded or CPU index out of range.
+            PermissionError: run this module as normal user.
+        """
+        if self.fd is None:
+            file_path = "/dev/cpu/%d/msr" % self.cpu
+            flags = os.O_RDWR
+            self.fd = os.open(file_path, flags)
+        os.lseek(self.fd, index, os.SEEK_SET)
+        if value is not None:
+            os.write(self.fd, value)
+        value_str = os.read(self.fd, 8)
+        (msr_value, ) = struct.unpack('<Q', value_str)
+        return msr_value
+    def __del__(self):
+        try:
+            os.close(self.fd)
+        except Exception:
+            pass
